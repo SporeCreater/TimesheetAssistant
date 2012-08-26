1d7053a6c32cea41f6795d0597cebf1c9b97fd02
fcuenca <fcuenca@9dc0e914-1b9d-fc40-ba95-8b55ba72b4d3>
2012-08-26 20:49:47 +0000

    git-svn-id: https://MSC-L3ALZ8R.allegisgroup.com/svn/TimesheetAssistant@9 9dc0e914-1b9d-fc40-ba95-8b55ba72b4d3

---
+++ trunk/PageDrivers.Tests/AppDriverTests.cs
@@ -0,0 +1,49 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PageDrivers.Tests
+{
+    [TestClass]
+    public class AppDriverTests
+    {
+        [TestMethod]
+        public void performs_login_into_the_application()
+        {
+            using (var p = new TimeSheetAppDriver(Config.APP_URL))
+            {
+                p.Login(Config.USER_NAME, Config.PASSWORD).Should().BeTrue();
+                p.LastErrorMessage.Should().BeEmpty();
+            }
+        }
+
+        [TestMethod]
+        public void fails_gracefully_when_cant_connect_to_web_app()
+        {
+            using(var p = new TimeSheetAppDriver(Config.APP_URL + "foo"))
+            {
+                p.Login(Config.USER_NAME, Config.PASSWORD).Should().BeFalse();
+                p.LastErrorMessage.Should().Contain("Page doesn't contain expected controls:");
+            }
+        }
+
+        [TestMethod]
+        public void fails_gracefully_when_authentication_fails()
+        {
+            using(var p = new TimeSheetAppDriver(Config.APP_URL))
+            {
+                p.Login("wrong user name", Config.PASSWORD).Should().BeFalse();
+                p.LastErrorMessage.Should().Contain("Login failed");
+            }
+        }
+
+        [TestMethod]
+        public void last_error_is_empty_by_default()
+        {
+            using (var p = new TimeSheetAppDriver(Config.APP_URL))
+            {
+                p.LastErrorMessage.Should().BeEmpty();
+            }
+        }
+
+    }
+}
---
+++ trunk/PageDrivers/TimeSheetAppDriver.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Interactors;
+using WatiN.Core;
+using WatiN.Core.Exceptions;
+
+namespace PageDrivers
+{
+    public class TimeSheetAppDriver : ILoginPage, IDisposable
+    {
+        private readonly IE _ie;
+
+        public TimeSheetAppDriver(string appUrl)
+        {
+            _ie = new IE(appUrl);
+            LastErrorMessage = string.Empty;
+        }
+
+        public void Close()
+        {
+            _ie.Close();
+        }
+
+        public bool Login(string userName, string password)
+        {
+            LastErrorMessage = string.Empty;
+
+            try
+            {
+                var p = new LoginPageDriver(_ie);
+
+                string pageVerificationResult = p.Verify();
+                if (pageVerificationResult != string.Empty)
+                {
+                    LastErrorMessage = "Page doesn't contain expected controls: " + pageVerificationResult;
+                    return false;
+                }
+
+                p.UserNameField.TypeText(userName);
+                p.PwdField.TypeText(password);
+                p.LoginButton.Click();
+
+                if (_ie.Url.Contains(LoginPageDriver.NextPageName))
+                {
+                    return true;
+                }
+                else
+                {
+                    LastErrorMessage = "Login failed";
+                    return false;
+                }
+
+            }
+            catch (ElementNotFoundException ex)
+            {
+                LastErrorMessage = "Unexpected page content: " + ex.Message;
+                return false;
+            }
+        }
+
+        public string LastErrorMessage { get; private set; }
+
+        public string CurrentWeek
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public void SelectCurrentWeek(string nextSaturday)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            _ie.Close();
+            _ie.Dispose();
+        }
+    }
+}
