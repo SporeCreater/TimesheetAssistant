a61c892bbc6e5f2a591a9b69d87733c78cf4317d
fcuenca <fcuenca@9dc0e914-1b9d-fc40-ba95-8b55ba72b4d3>
2012-08-26 20:50:44 +0000

    Sample for config file w/o credentials!!
    git-svn-id: https://MSC-L3ALZ8R.allegisgroup.com/svn/TimesheetAssistant@10 9dc0e914-1b9d-fc40-ba95-8b55ba72b4d3

---
+++ trunk/PageDrivers.Tests/Config - Sample.cs
@@ -0,0 +1,9 @@
+﻿namespace PageDrivers.Tests
+{
+    public static class Config
+    {
+        public static readonly string APP_URL = "https://timeandexpense.teksystems.com/webtime/";
+        public static readonly string USER_NAME = "USERNAME";
+        public static readonly string PASSWORD = "PASSWORD"; // DON'T CHECK IN YOUR PASSWORD!!
+    }
+}
